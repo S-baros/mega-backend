c9860a3723a02c3c6a68fbdd1e02d182ce22d327
Alexandre Schorm Barros <alexandre_barros@ufms.br>
2026-06-03 15:42:00 -0400

    fix: atualiza server

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-// server.js
 require('dotenv').config()
 const app = require('./src/app')
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`✅ Servidor rodando em http://localhost:${PORT}`)
-  console.log(`📄 Swagger disponível em http://localhost:${PORT}/api-docs`)
+  console.log(`Servidor rodando em http://localhost:${PORT}`)
+  console.log(`Swagger em http://localhost:${PORT}/api-docs`)
 })
